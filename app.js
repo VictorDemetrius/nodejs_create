184598fab934f9d9d6e29c3ad59abc9828487c69
Victor Demetrius Marqueto <73915190+VictorDemetrius@users.noreply.github.com>
2023-03-22 10:55:27 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-const express = require ("express")
+const express = require("express")
 const app = express()
-const handlebars = express ("express-handlebars").engine
+const handlebars = require("express-handlebars").engine
 
 app.engine("handlebars", handlebars({defaultLayout: "main"}))
-app.set("view engine", "handlebars")
+app.set("view engine" , "handlebars")
 
-app.get("/", function(req,res)
-{
+app.get("/", function(req,res){
     res.render("primeira_pagina")
 })
 
-app.listen(8081, function()
-{
-    console.log("SERVIDOR ATIVO") 
+app.listen(8081, function(){
+    console.log("Servidor ativo")
 })
---
+++ banco.js
@@ -0,0 +1,25 @@
+const Sequelize = require("sequelize")
+const sequelize = new Sequelize("test", "root", "",{
+    host: "localhost",
+    dialect: "mysql"
+})
+
+const Cadastro = sequelize.define("cadastros", {
+    nome:{
+        type: Sequelize.STRING
+    },
+    telefone:{
+        type: Sequelize.INTEGER
+    },
+    origem:{
+        type: Sequelize.STRING
+    },
+    data:{
+        type: Sequelize.DATE
+    },
+    observacao:{
+        type: Sequelize.STRING
+    }
+})
+
+ Cadastro.sync({force : true})
