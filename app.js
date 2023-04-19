aca2083123873b6a80abc554b2cf5e7a3ea39e08
Victor Demetrius Marqueto <73915190+VictorDemetrius@users.noreply.github.com>
2023-04-19 10:31:51 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.engine("handlebars", handlebars({defaultLayout: "main"}))
 app.set("view engine" , "handlebars")
 
 app.use(bodyParser.urlencoded({extends: false}))
-app.set(bodyParser.json())
+app.use(bodyParser.json())
 
 app.get("/", function(req,res){
     res.render("primeira_pagina")
@@ -22,7 +22,7 @@ app.get("/consulta", function(req,res){
     })
 })
 
-app.post("/cadastrar", function(req,res){
+app.post("/cadastrar", function(req, res){
     post.create({
         nome: req.body.nome,
         telefone: req.body.telefone,
@@ -36,6 +36,18 @@ app.post("/cadastrar", function(req,res){
     })
     
 })
+
+app.get("/excluir/:id", function(req,res){
+    post.destroy({
+        where: {'id': req.params.id},
+        force: true
+      }).then(function(){
+        res.redirect("/consulta")
+    }).catch(function(erro){
+        console.log("Erro ao carregar dados do banco: " +erro)
+    })
+})
+
 
 app.listen(8081, function(){
     console.log("Servidor Ativo!")
